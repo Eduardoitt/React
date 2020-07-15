909bbe43b0ce54fc31cad479f4d0ead5ff92aca9
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-07-14 21:02:24 -0700

    first session from react

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Begin whit React
+
+## Primera sesion 
+* Que es react
+* Conociendo las carpetas
+* Minuto 45
---
+++ primeraapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>Hola mundo</div>
+  );
+}
+
+export default App;

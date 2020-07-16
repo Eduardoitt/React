30cda2682ce0c7f09dea9af072645db5ae0eee42
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-07-15 21:09:27 -0700

    session 2

--- Readme.md
+++ Readme.md
@@ -1,6 +1,19 @@
 # Begin whit React
 
+## Iniciar proyecto
+
+npx create-react-app primeraapp
+cd primaeraapp
+npm start
+
 ## Primera sesion 
+### Seccion 1
 * Que es react
 * Conociendo las carpetas
-* Minuto 45
+
+## Segunda sesion
+### Seccion 2
+* Props o properties
+    * Son datos que recibo un componente a travez de un objeto
+### Seccion 3
+* Estado de componente state
--- primeraapp/src/App.js
+++ primeraapp/src/App.js
@@ -2,10 +2,51 @@ import React from 'react';
 
 import './App.css';
 
+//function Hellowworld(props){
+  //return(
+ // <div id="hello">
+    //<h3>{props.subtitle}</h3>
+    //{props.mytext}
+   // </div>
+ // )
+//}
+class Hellowworld extends React.Component{
+  state={
+    show:true
+  }
+  toggleShow=()=>{
+    this.setState({show:!this.state.show})
+  }
+  render(){
+    if(this.state.show){
+      return(
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.mytext}
+        <input onClick={this.toggleShow}type="button" value="Change state"></input>
+        </div>
+      )
+    }else{
+      return <h1>no hay nada para mostrar
+              <input onClick={this.toggleShow}type="button" value="Change state"></input>
+      </h1>
+
+    }
+  }
+}
+// funcion flecha
+//const App = () => <div>This is my function: <Hellowworld/> </div>
+
 function App() {
   return (
-    <div>Hola mundo</div>
+    <div>This is my component: 
+      <Hellowworld mytext="Hello Eduardo" subtitle="engineer"/>
+      <Hellowworld mytext="23 years"/>
+      <Hellowworld mytext="from Tijuana"/></div>
   );
 }
-
+// clase
+//class App extends React.Component{
+//return <div>This is my compinent: <Hellowworld/></div>
+//}
 export default App;
